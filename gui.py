5943d5820a836641020de730876ca3a6b9743826
Joel <jwf.hoffmann@gmx.de>
2021-12-01 19:17:04 +0100

    Hilfe-Dialog hinzugefügt

--- gui.py
+++ gui.py
@@ -4,12 +4,16 @@ from pygame import surface;
 from maze import Maze as Model;
 from maze_display import Maze;
 import style;
+pg.init();
 
 DIM = (10, 10);
 FOOD_COUNT = 1;
 
+FONT = pg.font.SysFont("times", int(style.S_FOOT * 0.75));
 render_count = 0;
 dir_helper = (pg.K_UP, pg.K_RIGHT, pg.K_DOWN, pg.K_LEFT);
+ICON = pg.image.load("yammie.png");
+pg.display.set_icon(ICON);
 
 TEXT = "";
 def update_std_text ():
@@ -27,22 +31,20 @@ def render ():
     render_count += 1;
     # print("RENDER #" + str(render_count));
     SCREEN.blit(maze.render(), (0, 0));
-    font = pg.font.SysFont("times", int(style.S_FOOT * 0.75));
-    text = font.render(TEXT, True, "#eeeeee", "#444444");
+    text = FONT.render(TEXT, True, "#eeeeee", "#444444");
     pg.draw.rect(SCREEN, "#444444", (0, FENSTER[1] - style.S_FOOT, FENSTER[0], style.S_FOOT));
     SCREEN.blit(text, (0, FENSTER[1] - (style.S_FOOT * 0.9)))
 
     pg.display.flip();
 
-def format_input(newdim, cursor):
-    return f' Neu: {newdim[0]}{"_" if not cursor else ""} x {(newdim[1] + "_") if cursor else ""}';
-
 def input_new ():
-    newdim = ["", ""];
+    def subrender():
+        global TEXT;
+        TEXT = f' Neu: {newdim[0]}{"_" if not cursor else ""} x {newdim[1] + ("_" if cursor == 1 else "")}  Essen: {newdim[2]}{"_" if cursor == 2 else ""}';
+        render();
+    newdim = ["", "", ""];
     cursor = 0;
-    global TEXT;
-    TEXT = format_input(newdim, cursor);
-    render();
+    subrender();
     weiter = True;
     while weiter:
         uhr.tick(40);
@@ -52,36 +54,62 @@ def input_new ():
                 elif (e.key == pg.K_RETURN):
                     if (newdim[cursor]):
                         cursor += 1;
-                        weiter = cursor == 1;
-                        TEXT = format_input(newdim, cursor); render();
+                        weiter = cursor < 3;
+                        subrender();
                     else:
                         TEXT = "EINGABE BITTE!"; render();
                         pg.time.wait(1000);
-                        TEXT = format_input(newdim, cursor); render();
+                        subrender();
                 elif (e.key in range(48, 58)):
                     newdim[cursor] += str(e.key - 48);
-                    TEXT = format_input(newdim, cursor); render();
-    if (cursor == 2):
-        init(int(newdim[0]), int(newdim[1]));
+                    subrender();
+                elif (e.key == pg.K_BACKSPACE):
+                    if (newdim[cursor]): newdim[cursor] = newdim[cursor][:-1];
+                    else: cursor -= 1;
+                    if (cursor == -1): weiter = False;
+                    else: subrender();
+                elif (e.key == pg.K_LEFT and cursor):
+                    cursor -= 1;
+                    subrender();
+                elif (e.key == pg.K_RIGHT and cursor < 2):
+                    cursor += 1;
+                    subrender();
+
+    if (cursor == 3):
+        global DIM, FOOD_COUNT;
+        DIM = (int(newdim[0]), int(newdim[1]))
+        FOOD_COUNT = int(newdim[2])
+        init();
     else: #Wurde abgbrochen
         update_std_text();
         render();
 
-def init(x: int, y: int):
+def init():
     global maze, FENSTER, SCREEN, uhr, steps, YAMMIE;
-    maze = Maze(Model(x, y, True));
-    maze.model.mkfood(FOOD_COUNT);
+    maze = Maze(Model(DIM[0], DIM[1], True));
+    maze.model.mkfood(FOOD_COUNT % ((DIM[0] * DIM[1]) - 1));
     FENSTER = (maze.grössen[0], maze.grössen[1] + style.S_FOOT);
     SCREEN = pg.display.set_mode(FENSTER);
-    pg.display.set_caption(f'Maze {x} x {y}');
+    pg.display.set_caption(f'Maze {DIM[0]} x {DIM[1]}');
     uhr = pg.time.Clock();
     steps = 0;
-    YAMMIE = pg.transform.smoothscale(pg.image.load("yammie.png"), (min(*FENSTER), min(*FENSTER)));
+    YAMMIE = pg.transform.smoothscale(ICON, (min(*FENSTER), min(*FENSTER)));
     update_std_text();
     render();
 
-pg.init();
-init(*DIM);
+def help(start):
+    SCREEN = pg.display.set_mode((400, 400));
+    if (start): pg.display.set_caption("Willkommen bei MAZE!");
+    uhr = pg.time.Clock();
+    SCREEN.blit(pg.transform.smoothscale(pg.image.load("start.png" if start else "help.png"), (400, 400)), (0, 0));
+    pg.display.flip();
+    weiter = True;
+    while weiter:
+        uhr.tick(40);
+        for e in pg.event.get(): weiter = not (e.type == pg.KEYDOWN and e.key == pg.K_RETURN);
+
+help(True);
+init();
 
 weiter = True;
 while weiter:
@@ -100,7 +128,8 @@ while weiter:
                 maze.stein = list(maze.model.entry);
                 render();
             elif (e.key == pg.K_ESCAPE): input_new();
-            elif (e.key == pg.K_r): init(maze.model.x, maze.model.y);
+            elif (e.key == pg.K_r): init();
+            elif (e.key == pg.K_h): help(False); init();
         else: continue;  
 
 print(f'QUIT AFTER {render_count} RENDER CYCLES.')
